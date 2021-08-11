79ec0ebd9024a86fdea718fe4994765e3512880a
Imran Ahmed M (21222) <84715478+immuid@users.noreply.github.com>
2021-08-11 19:55:19 +0530

    Update JavaMail.java

--- mailProject/JavaMail.java
+++ mailProject/JavaMail.java
@@ -1,23 +1,25 @@
 package mailProject;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Set;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class JavaMail {
+class JavaMail{
 	public static void main(String[] args) throws Exception {
+		JavaMailUtil bmail=new JavaMailUtil();
+		Set<String> emails = get();
+	
+  for (String string:emails) {
+	  		bmail.process(string);
+}
+  }
+	private static Set<String> get(){
 		
-		String s[]=new String[3];
-		s[0]="imranhash510@gmail.com";
-		s[1]="abdulthedon007@gmail.com";
-		s[2]="immuimran2k@gmail.com";
+		Set<String> emails = new HashSet<>();
+		 ResourceBundle rb=ResourceBundle.getBundle("password",new Locale("en"));// Locale.getdefault()//used properties 
+		emails.add(rb.getString("e1"));//email is stored in properties
+		emails.add(rb.getString("e2"));//email is stored in properties
+		return emails;
 		
-		for(String str:s) {
-			JavaMailUtil.sendMails(str);
-		}
-		
-		//JavaMailUtil.sendMails("");
-		//JavaMailUtil.sendMails("imranhash510@gmail.com");
-		//JavaMailUtil.sendMails("abdulthedon007@gmail.com");
 	}
-
 }
