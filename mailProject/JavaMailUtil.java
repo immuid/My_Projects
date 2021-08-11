2b47bb54d2a2555e5175f29444d6cd43a28a5cb7
Imran Ahmed M (21222) <84715478+immuid@users.noreply.github.com>
2021-08-11 19:56:09 +0530

    Update JavaMailUtil.java

--- mailProject/JavaMailUtil.java
+++ mailProject/JavaMailUtil.java
@@ -1,7 +1,8 @@
 package mailProject;
 
+import java.util.Locale;
 import java.util.Properties;
-
+import java.util.ResourceBundle;
 
 import javax.mail.Authenticator;
 import javax.mail.Message;
@@ -12,6 +13,13 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class JavaMailUtil {
+	
+	public void process(String string) throws Exception{
+		sendMails(string);
+		prepareMessage(null, string, string);
+		
+	}
+
  public static void sendMails(String recipient) throws Exception {
 	 System.out.println("Preparing to send Email...");
 	 Properties properties=new Properties();
@@ -26,8 +34,11 @@ public class JavaMailUtil {
 	 properties.put("mail.smtp.host","smtp.gmail.com");
 	 properties.put("mail.smtp.port","587");
 	 
-	 String myAccountEmail=" ********@gmail.com"; //enter the mail id
-	 String Password =" **********";//enter the Password 
+	 ResourceBundle rb=ResourceBundle.getBundle("password",new Locale("en"));// Locale.getdefault()
+		
+	 
+	 String myAccountEmail="imranhash510@gmail.com";
+	 String Password =rb.getString("p1");  //used properties
 	 
 	 Session session = Session.getInstance(properties,new Authenticator() {
 		 @Override
@@ -36,25 +47,43 @@ public class JavaMailUtil {
 		}
 	
 	 });
-	 Message message = prepareMessage(session,myAccountEmail, recipient);
+	 Message message = prepareMessage(session,myAccountEmail,recipient);
 	 
 	Transport.send(message);
 	System.out.println("Email sent Successfully...");
  }
 
-private static Message prepareMessage(Session session,String myAccountEmail,String recipient) {
+public static Message prepareMessage(Session session,String myAccountEmail,String recipient) {
+	
+	Message message=new MimeMessage(session);
+	
 	try {
-		Message message=new MimeMessage(session);
+	
 		message.setFrom(new InternetAddress(myAccountEmail) );
 		message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
 		message.setSubject("My First Email From Java App");
-		message.setText("Hey there, \n look my Email" );
-		return message;
+		
+		String[] splittedArray = recipient.split("@");
+		String name = splittedArray[0];
+	//	System.out.println("Message sent to..:"+name);
+		
+		message.setText("Dear Mr." +name
+				
+				+ ",\n Greetings To You,\n" 
+				+ "Welcome to my GitHub Account:https://github.com/immuid/JAVA-programs.\n" 
+				+ "\nThanks & Regards,\n" 
+				+ "Mr.Imran Ahmed, FullStack Developement Trainee,\n"  
+				+ "Harris Infotech Pvt.Ltd,\n" 
+				+ " E-mail:imranhash510@gmail.com,\n" 
+				+"Phone: +91-8072946976 ");
+		
 		
 	} catch (Exception e) {
-		
+		e.printStackTrace();
 	}
 	
-	return null;
+	return message;
 }
+
+
 }
